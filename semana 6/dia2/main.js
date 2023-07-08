71286a891d36fde62ad3b242e1b8968dddf05d31
alexis-moing <135295210+alexis-Moing@users.noreply.github.com>
2023-07-07 19:52:36 -0500

    actualizando semana 6

---
+++ semana 6/dia2/main.js
@@ -0,0 +1,10 @@
+let nombre = "alex";
+let edad = 20;
+
+console.log("mi nombre es", nombre);
+
+let dobleedad = edad * 2;
+let tripleedad = edad * 3;
+
+console.log("mi edad es", edad , "su doble es", dobleedad)
+console.log("el triple de mi edad es", tripleedad)
